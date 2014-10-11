0e1e47364ad28d02f07c387b2cf0625565edd397
ob <obnoxious@dongcorp.org>
2014-10-11 05:13:29 +0000

    gogogo

--- viewbot.c
+++ viewbot.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <curl/curl.h>
 
-int main(void)
+int main(int argc, char *args[])
 {
   CURL *curl;
   CURLcode res;
 
+  printf("[+] Tested Stuff For: %s\n", args[1]);
+
   curl = curl_easy_init();
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://google.com/");
-    /* example.com is redirected, so we tell libcurl to follow redirection */
+    char url[100];
+    snprintf(url, 99, "http://api.twitch.tv/api/channels/%s/access_token", args[1]);
+    curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-    /* Perform the request, res will get the return code */
     res = curl_easy_perform(curl);
-    /* Check for errors */
     if(res != CURLE_OK)
       fprintf(stderr, "curl_easy_perform() failed: %s\n",
               curl_easy_strerror(res));
-
-    /* always cleanup */
     curl_easy_cleanup(curl);
   }
   return 0;
