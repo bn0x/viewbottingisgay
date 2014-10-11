976d0693488ab8ff3486090b015a9cfb3c5c7722
ob <obnoxious@dongcorp.org>
2014-10-11 05:48:20 +0000

    oh my gfucking god

--- viewbot.c
+++ viewbot.c
@@ -1,24 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
+
+struct MemoryStruct {
+  char *memory;
+  size_t size;
+};
+
+
+static size_t
+WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+  if(mem->memory == NULL) {
+    /* out of memory! */
+    printf("not enough memory (realloc returned NULL)\n");
+    return 0;
+  }
+
+  memcpy(&(mem->memory[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+
+  return realsize;
+}
+
 
 int main(int argc, char *args[])
 {
   CURL *curl;
   CURLcode res;
 
+  struct MemoryStruct chunk;
+
+  chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
+  chunk.size = 0;    /* no data at this point */
+
   printf("[+] Tested Stuff For: %s\n", args[1]);
 
   curl = curl_easy_init();
   if(curl) {
     char url[100];
+    char access_token_data[200];
     snprintf(url, 99, "http://api.twitch.tv/api/channels/%s/access_token", args[1]);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
     res = curl_easy_perform(curl);
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
+    printf("[+] Got access_token data..\n");
     curl_easy_cleanup(curl);
+    printf("%s", chunk);
   }
   return 0;
 }
